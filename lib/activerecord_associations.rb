1d114a9159990a71cb34ffe6f73fb0b516b881dc
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-06-14 12:02:18 -0700

    initial commit

---
+++ lib/activerecord_associations.rb
@@ -0,0 +1,6 @@
+require "activerecord_associations/version"
+
+module ActiverecordAssociations
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/activerecord_associations_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ActiverecordAssociations do
+  it "has a version number" do
+    expect(ActiverecordAssociations::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
